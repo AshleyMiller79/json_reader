5162250b02cbb8046857c4134c1b147e782394a2
ipekbilir <81853731+ipekbilir@users.noreply.github.com>
2021-06-16 19:25:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
+var showList = [];
+
 function getData() {
   return fetch("../data/tv-shows.json").then((resp) => resp.json());
 }
-
-getData().then((data) => {
+function setTvShows(data) {
   var liste = document.querySelector(".tvShowList");
 
+  liste.innerHTML = "";
   data.forEach((tvShow) => {
-    console.log(tvShow);
-    liste.innerHTML += `   
- <div className="card" style={{ width: "16rem" }}>
-   <img src=${tvShow.show.image.medium} >
-   <div className="card-body">
-     <h5 className="card-title">${tvShow.show.name}</h5>
-     <p className="card-text">
-     ${tvShow.show.genres} ${tvShow.show.status} ${tvShow.show.summary}
-     </p>
+    liste.innerHTML += ` 
+    <div class="col-md-3"> 
+ <div class="card" >
+   <img src=${
+     tvShow.show.image ? tvShow.show.image.medium : ""
+   } class="card-img-top">
+   <div class="card-body">
+     <h5 class="card-title">${tvShow.show.name}</h5>
      
-     <a className= "btn btn-success"  href=${tvShow.show.url}>Detaylar</a>
+     
+     <a class= "btn btn-success"  href=${tvShow.show.url}>Detaylar</a>
      <p> </p>
    </div>
- </div>`;
+ </div></div> `;
   });
+}
+document.getElementById("search").oninput = (e) => {
+  var text = e.target.value.toLowerCase();
+  var data = showList.filter((item) => {
+    return item.show.name.toLowerCase().includes(text);
+  });
+  setTvShows(data);
+};
+
+getData().then((data) => {
+  showList = data;
+  setTvShows(showList);
 });
